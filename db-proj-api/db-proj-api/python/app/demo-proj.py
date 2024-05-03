cbe5bfabbd9f33c0db3f5bbb687458f680c81523
JanviN625 <janvinandwani@gmail.com>
2024-05-03 15:29:11 -0400

    update auction old potential fix

--- db-proj-api/db-proj-api/python/app/demo-proj.py
+++ db-proj-api/db-proj-api/python/app/demo-proj.py
@@ -520,13 +520,14 @@ def edit_auction(auctionId, userId):
         return flask.jsonify(response)
 
     # parameterized queries, good for security and performance
-    statement = 'INSERT INTO old_auction (auctionid, auctiontitle, auction_end, sellerdesc, users_userid, items_itemid) WHERE auctionid = %s'
-    values = (auctionId,)
+    ostatement = 'INSERT INTO old_auction (auctionid, auctiontitle, auction_end, sellerdesc, users_userid, items_itemid) WHERE auctionid = %s'
+    ovalues = (auctionId,)
 
     statement = 'UPDATE auction SET auction_end = %s, sellerdesc = %s WHERE auctionid = %s'
     values = (payload['auction_end'], payload['sellerdesc'], auctionId)
 
     try:
+        cur.execute(ostatement, ovalues)
         cur.execute(statement, values)
 
         conn.commit()
